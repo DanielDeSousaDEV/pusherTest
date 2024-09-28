878155bf88c2b487234be0bf66b999988c9b5cfc
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2024-09-28 19:59:56 -0300

    criação de uma nova variável de ambiente

--- server/config/conn.php
+++ server/config/conn.php
@@ -1,6 +1,13 @@
 <?php 
     try {
-        $conn = new PDO($_ENV['DB_DNS'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        $fullDNS = $_ENV['DB_DNS'] . ';dbname=' . $_ENV['DB_NAME'];
+
+        $conn = new PDO(
+            $fullDNS,
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASSWORD']
+        );
+
     } catch (PDOException $e) {
         echo '<strong>Erro no PDO:</strong>' . $e;
     }
